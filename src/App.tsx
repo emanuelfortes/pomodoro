1257e261d7a919b94a1ca00f0f00dffbbb7cc799
Emanuel Fortes <114531262+emanuelfortes@users.noreply.github.com>
2026-04-09 00:31:20 -0300

    subindo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import './styles/theme.css';
+import './styles/global.css';
+
 export function App() {
-  console.log('oi');
-  return <h1>Olá Mundo (do App)</h1>
-  
+  return (
+    <>
+      
+    </>
+  );
 }
