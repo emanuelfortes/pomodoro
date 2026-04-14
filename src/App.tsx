73312fe954e8cbafe788c0a9bd1ff3c927bf3254
Emanuel Fortes <114531262+emanuelfortes@users.noreply.github.com>
2026-04-13 21:40:15 -0300

    aula 21

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
-import './styles/theme.css';
-import './styles/global.css';
+import "./styles/theme.css";
+import "./styles/global.css";
 
 export function App() {
   return (
     <>
-      
+
+        <div className="container">
+          <div className="content">
+            <section>
+              LOGO
+            </section>
+          </div>
+        </div>
+
+        <div className="container">
+          <div className="content">
+            <section>
+              MENU
+            </section>
+          </div>
+        </div>
+        <div className="container">
+          <div className="content">
+            <section>
+              FORM
+            </section>
+          </div>
+        </div>
+        <div className="container">
+          <div className="content">
+            <section>
+              FOOTER 
+            </section>
+          </div>
+        </div>
     </>
   );
 }
